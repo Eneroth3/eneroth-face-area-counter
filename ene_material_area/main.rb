dd4efc661ab90014f39a37ca741bc9ca8abab55a
Eneroth3 <eneroth3@gmail.com>
2017-11-23 14:05:15 +0100

    Sort methods alphabetically

--- ene_material_area/main.rb
+++ ene_material_area/main.rb
@@ -20,27 +20,48 @@ module Eneroth::MaterialAreaCounter
     vector * arbitrary_non_parallel_vector(vector).normalize
   end
 
-  # Determine the normal vector for a plane.
+  # Count the areas of the materials in model and show to the user.
   #
-  # @param [Array(Geom::Point3d, Geom::Vector3d), Array(Float, Float, Float, Float)]
-  # @return [Geom::Vector3d]
-  def self.plane_normal(plane)
-    return plane[1].normalize if plane.size == 2
-    a, b, c, _ = plane
+  # @return [void]
+  def self.count_material_areas
 
-    Geom::Vector3d.new(a, b, c).normalize
+    # REVIEW: What happens when user is not in the model root? That messes up
+    # the Transformations and coordinates reported, doesn't it?
+    areas = iterate_entities(Sketchup.active_model.entities)
+
+    areas = areas.map { |k, v|
+      [format_material_name(k), Sketchup.format_area(v)]
+    }.to_h
+
+    # TODO: Format string nicely.
+    UI.messagebox(
+      format_hash(areas),
+      MB_MULTILINE,
+      EXTENSION.name
+    )
+
+    nil
   end
 
-  # Compute the area scale factor from transformation at a plane.
+  # Format hash.
   #
-  # @param [Array(Geom::Point3d, Geom::Vector3d), Array(Float, Float, Float, Float)]
-  # @return [Float]
-  def self.scale_factor_in_plane(plane, transformation)
-    normal = plane_normal(plane)
-    plane_vector0 = arbitrary_perpendicular_vector(normal)
-    plane_vector1 = plane_vector0 * normal
+  # @param [Hash]
+  # @return [String]
+  def self.format_hash(hash)
+    key_length = hash.keys.map(&:length).max
+    value_length = hash.values.map(&:length).max
 
-    (plane_vector0.transform(transformation) * plane_vector1.transform(transformation)).length.to_f
+    hash.map { |k, v|
+      "#{k.ljust(key_length)}: #{v.rjust(value_length)}"
+    }.join("\n")
+  end
+
+  # Get name of material or pre-defined default material name for nil.
+  #
+  # @param [Sketchup::Material, nil]
+  # @return [String]
+  def self.format_material_name(material)
+    material ? material.display_name : DEFAULT_MATERIAL_NAME
   end
 
   # Iterate Entities collection to sum up areas for each material.
@@ -70,48 +91,27 @@ module Eneroth::MaterialAreaCounter
     areas
   end
 
-  # Format hash.
+  # Determine the normal vector for a plane.
   #
-  # @param [Hash]
-  # @return [String]
-  def self.format_hash(hash)
-    key_length = hash.keys.map(&:length).max
-    value_length = hash.values.map(&:length).max
+  # @param [Array(Geom::Point3d, Geom::Vector3d), Array(Float, Float, Float, Float)]
+  # @return [Geom::Vector3d]
+  def self.plane_normal(plane)
+    return plane[1].normalize if plane.size == 2
+    a, b, c, _ = plane
 
-    hash.map { |k, v|
-      "#{k.ljust(key_length)}: #{v.rjust(value_length)}"
-    }.join("\n")
+    Geom::Vector3d.new(a, b, c).normalize
   end
 
-  # Get name of material or pre-defined default material name for nil.
+  # Compute the area scale factor from transformation at a plane.
   #
-  # @param [Sketchup::Material, nil]
-  # @return [String]
-  def self.format_material_name(material)
-    material ? material.display_name : DEFAULT_MATERIAL_NAME
-  end
+  # @param [Array(Geom::Point3d, Geom::Vector3d), Array(Float, Float, Float, Float)]
+  # @return [Float]
+  def self.scale_factor_in_plane(plane, transformation)
+    normal = plane_normal(plane)
+    plane_vector0 = arbitrary_perpendicular_vector(normal)
+    plane_vector1 = plane_vector0 * normal
 
-  # Count the areas of the materials in model and show to the user.
-  #
-  # @return [void]
-  def self.count_material_areas
-
-    # REVIEW: What happens when user is not in the model root? That messes up
-    # the Transformations and coordinates reported, doesn't it?
-    areas = iterate_entities(Sketchup.active_model.entities)
-
-    areas = areas.map { |k, v|
-      [format_material_name(k), Sketchup.format_area(v)]
-    }.to_h
-
-    # TODO: Format string nicely.
-    UI.messagebox(
-      format_hash(areas),
-      MB_MULTILINE,
-      EXTENSION.name
-    )
-
-    nil
+    (plane_vector0.transform(transformation) * plane_vector1.transform(transformation)).length.to_f
   end
 
   menu = UI.menu("Plugins")
